c42091f0f5305c471f0a3607455e632de0147e3c
uec203 <uec20342@gmail.com>
2024-07-21 14:14:26 +0530

    Assignment-6

--- Assignment-7/CaptilizeTheFirstWord.java
+++ Assignment-7/CaptilizeTheFirstWord.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
-
+/*1. Create a program to count vowels in a given string using string functions
+2. Write a program to reverse the characters of a string.
+3. Write a program to reverse the words in a sentence.
+4. Write a program to validate email address (contains @ and proper domain format). Note: Don't use regular expressions
+5. Write a program to capitalize the first letter of each words in a sentence.
+6. https://exercism.org/tracks/java/exercises/squeaky-clean
+*/
 public class CaptilizeTheFirstWord {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
